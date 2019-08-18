eb1faf3ead821925bed75d6868537d710471e1e0
Ali898989 <42411309+Ali898989@users.noreply.github.com>
2019-08-18 17:08:47 +0200

    Add files via upload

---
+++ setup5.sh
@@ -0,0 +1,426 @@
+
+g='\033[1;32m'
+gg='\033[5;32m'
+ggg='\033[7;32m'
+gggg='\033[0;32m'
+p='\033[1;35m'
+pp='\033[5;35m'
+ppp='\033[7;35m'
+pppp='\033[0;35m'
+y='\033[1;33m'
+yy='\033[5;33m'
+yyy='\033[7;33m'
+yyyy='\033[0;33m'
+r='\033[1;31m'
+rr='\033[5;31m'
+rrr='\033[7;31m'
+rrrr='\033[0;31m'
+b='\033[1;34m'
+bb='\033[5;34m'
+bbb='\033[7;34m'
+bbbb='\033[0;34m'
+re='\033[0m'
+c='\033[1;36m'
+cc='\033[5;36m'
+ccc='\033[7;36m'
+cccc='\033[0;36m'
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+purple='\033[1;35m'
+reset='\033[0m'
+y='\033[1;33m'
+
+
+cd
+clear
+termux-setup-storage
+rm -rf $HOME/payload5.zip
+rm -rf $HOME/payload5
+rm -rf $HOME/payload6.zip
+rm -rf $HOME/payload7.zip
+rm -rf $HOME/../usr/bin/payload.sh
+rm -rf $HOME/../usr/bin/payload
+rm -rf $HOME/../usr/bin/payload6
+rm -rf $HOME/../usr/bin/payload7
+rm -rf $HOME/../usr/bin/setup5.sh
+clear
+echo -e "$c ali.max = $g https://www.facebook.com/ali.max.796774  "
+echo -e "$g"
+read -p "               -------(entar)------"
+clear
+
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'>              '$g']\'
+sleep 0.4
+clear
+
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'->             '$g']|'
+sleep 0.4
+clear
+
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'-->            '$g']*'
+sleep 0.4
+clear
+
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'--->           '$g']\'
+sleep 0.4
+clear
+
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'---->          '$g']|'
+
+clear
+
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'----->         '$g']*'
+
+
+clear
+
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'------>        '$g']|'
+clear
+
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'------>        '$g']\'
+clear
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'------>        '$g']*'
+clear
+
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'------->       '$g']|'
+clear
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'-------->      '$g']*'
+rm nn.txt
+clear
+
+echo -e 'Wa'$p'/'
+clear
+
+echo -e $g 'Please Wait ===+['$p'--------->     '$g']\'
+sleep 0.6
+clear
+
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'---------->    '$g']|'
+sleep 0.5
+clear
+
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'----------->   '$g']*'
+sleep 0.4
+clear
+
+echo -e 'Wa'$p'/'
+clear
+
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
+sleep 0.4
+clear
+
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
+sleep 0.4
+clear
+
+echo -e 'Wa'$p'/'
+clear
+sleep 0.2
+
+
+
+
+
+
+if [ -s /data/data/com.termux/files/usr/bin/wget ]; then
+echo " "
+else
+echo -e "$c --------($r wget$c )--------"
+sleep 2
+pkg install wget -y > nn.txt
+clear
+fi
+if [ -s /data/data/com.termux/files/usr/bin/git ]; then
+echo ""
+else
+echo -e "$c --------($r git$c )--------"
+sleep 2
+pkg install git -y > nn.txt
+clear
+fi
+
+if [ -s /data/data/com.termux/files/usr/bin/php ]; then
+echo ""
+else
+echo -e "$c --------($r php$c )--------"
+sleep 2
+pkg install php -y > nn.txt
+clear
+fi
+if [ -s /data/data/com.termux/files/usr/bin/python ]; then
+echo ""
+else
+echo -e "$c --------($r python$c )--------"
+sleep 2
+pkg install python -y > nn.txt
+clear
+fi
+if [ -s /data/data/com.termux/files/usr/bin/python2 ]; then
+echo ""
+else
+echo -e "$c --------($r python2$c )--------"
+sleep 2
+pkg install python2 -y > nn.txt
+clear
+fi
+
+if [ -s /data/data/com.termux/files/usr/bin/curl ]; then
+echo ""
+else
+echo -e "$c --------($r curl$c )--------"
+sleep 2
+pkg install curl -y > nn.txt
+clear
+fi
+if [ -s /data/data/com.termux/files/usr/bin/nano ]; then
+echo ""
+else
+echo -e "$c --------($r nano$c )--------"
+sleep 2
+pkg install nano -y > nn.txt
+clear
+fi
+
+if [ -s /data/data/com.termux/files/usr/bin/zip ]; then
+echo ""
+else
+echo -e "$c --------($r zip$c )--------"
+sleep 2
+pkg install zip -y > nn.txt
+clear
+fi
+
+if [ -s /data/data/com.termux/files/usr/bin/unzip ]; then
+echo ""
+else
+echo -e "$c --------($r unzip$c )--------"
+sleep 2
+pkg install unzip -y > nn.txt
+clear
+fi
+if [ -s /data/data/com.termux/files/usr/bin/figlet ]; then
+echo ""
+else
+echo -e "$c --------($r figlet$c )--------"
+sleep 2
+pkg install figlet -y > nn.txt
+
+clear
+fi
+if [ -s /data/data/com.termux/files/usr/bin/nmap ]; then
+echo ""
+else
+echo -e "$c --------($r nmap$c )--------"
+sleep 2
+pkg install nmap -y > nn.txt
+clear
+fi
+
+clear
+
+
+
+
+
+ss(){
+echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
+cd /sdcard 
+rm -rif payload5
+git clone https://github.com/alimaxali/payload5
+mkdir /sdcard/payload5
+mkdir /sdcard/payload5/Encrypt
+clear
+if [ -s /sdcard/payload5/payload7.zip ]; then
+#read -p "payload7.zip------(copy)------>/sdcard/payload5 :"
+echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
+cd
+mkdir payload5
+
+mv /sdcard/Download/payload7.zip $HOME/payload5
+mv /sdcard/payload5/payload7.zip $HOME/payload5
+cd payload5
+unzip payload7.zip
+echo "" 
+echo "" 
+echo "" 
+read -p "---------> "
+cd $HOME/payload5 && chmod +x *
+cd $HOME/payload5 && chmod +x */*
+cd $HOME/payload5 && chmod +x .*/*
+cd $HOME/payload5 && chmod +x .*/*/*
+cd $HOME/payload5 && chmod +x .*/*/*/*
+cd
+cp $HOME/payload5/payload.sh $HOME/../usr/bin/payload7
+cp $HOME/payload5/payload.sh $HOME/../usr/bin/payload.sh
+cp $HOME/payload5/payload.sh $HOME/../usr/bin/payload
+#cp $HOME/payload6/.setup5.sh $HOME/../usr/bin/setup5.sh
+chmod +x $HOME/../usr/bin/setup5.sh
+cd
+rm -rf setup55
+cd
+rm -rf $HOME/payload7.zip
+rm -rf $HOME/payload5/payload7.zip
+cd $HOME/payload5
+chmod +x .*
+cd $HOME/payload5/.tool
+chmod +x */*
+clear
+
+echo -e 'Wa'$p'/'
+clear
+
+echo -e "$g+++++++++++>[$pWelcome to the new update$p$g]<+++++++++++++"
+echo -e "     Hello      "
+echo -e "     $p     new "
+echo -e "     $g         Update "
+sleep 2
+payload
+clear
+else
+echo -e "$r"
+figlet -f big "ERROR"
+echo "     "
+echo -e "     $y no----> $r Internet❌"
+echo -e "    $y($g 1$y )$b Check the internet"
+echo -e "    $y($g 2$y )$b Check the Storage space"
+fi
+}
+
+
+
+if [ -s /data/data/com.termux/files/usr/bin/wget ]; then
+echo -e "$c --------($g wget$c )--------"
+else
+echo -e "$c --------($r wget$c )--------"
+sleep 2
+fi
+sleep 2
+if [ -s /data/data/com.termux/files/usr/bin/git ]; then
+echo -e "$c --------($g git$c )--------"
+else
+echo -e "$c --------($r git$c )--------"
+sleep 2
+fi
+sleep 2
+if [ -s /data/data/com.termux/files/usr/bin/php ]; then
+echo -e "$c --------($g php$c )--------"
+else
+echo -e "$c --------($r php$c )--------"
+sleep 2
+fi
+sleep 2
+if [ -s /data/data/com.termux/files/usr/bin/python ]; then
+echo -e "$c --------($g python$c )--------"
+else
+echo -e "$c --------($r python$c )--------"
+sleep 2
+fi
+sleep 2
+if [ -s /data/data/com.termux/files/usr/bin/python2 ]; then
+echo -e "$c --------($g python2$c )--------"
+else
+echo -e "$c --------($r python2$c )--------"
+sleep 2
+fi
+sleep 2
+
+if [ -s /data/data/com.termux/files/usr/bin/curl ]; then
+echo -e "$c --------($g curl$c )--------"
+else
+echo -e "$c --------($r curl$c )--------"
+sleep 2
+fi
+sleep 2
+if [ -s /data/data/com.termux/files/usr/bin/nano ]; then
+echo -e "$c --------($g nano$c )--------"
+else
+echo -e "$c --------($r nano$c )--------"
+sleep 2
+fi
+sleep 2
+
+if [ -s /data/data/com.termux/files/usr/bin/zip ]; then
+echo -e "$c --------($g zip$c )--------"
+else
+echo -e "$c --------($r zip$c )--------"
+sleep 2
+fi
+sleep 2
+
+if [ -s /data/data/com.termux/files/usr/bin/unzip ]; then
+echo -e "$c --------($g unzip$c )--------"
+else
+echo -e "$c --------($r unzip$c )--------"
+sleep 2
+fi
+sleep 2
+if [ -s /data/data/com.termux/files/usr/bin/figlet ]; then
+echo -e "$c --------($g figlet$c )--------"
+else
+echo -e "$c --------($r figlet$c )--------"
+sleep 2
+fi
+sleep 2
+if [ -s /data/data/com.termux/files/usr/bin/nmap ]; then
+echo -e "$c --------($g nmap$c )--------"
+else
+echo -e "$c --------($r nmap$c )--------"
+sleep 2
+fi
+if [ -s /data/data/com.termux/files/usr/bin/nmap ] &&
+   [ -s /data/data/com.termux/files/usr/bin/figlet ] &&
+   [ -s /data/data/com.termux/files/usr/bin/unzip ] &&
+   [ -s /data/data/com.termux/files/usr/bin/zip ] &&
+   [ -s /data/data/com.termux/files/usr/bin/nano ] &&
+   [ -s /data/data/com.termux/files/usr/bin/curl ] &&
+   [ -s /data/data/com.termux/files/usr/bin/python2 ] &&
+   [ -s /data/data/com.termux/files/usr/bin/python ] &&
+   [ -s /data/data/com.termux/files/usr/bin/php ] &&
+   [ -s /data/data/com.termux/files/usr/bin/git ] &&
+   [ -s /data/data/com.termux/files/usr/bin/wget ]; then
+ss
+else
+echo -e "$r"
+figlet -f big "ERROR"
+echo "     "
+echo -e "     $y no----> $r Internet❌"
+echo -e "    $y($g 1$y )$b Check the internet"
+echo -e "    $y($g 2$y )$b Check the Storage space"
+sleep 2
+fi
+
+
